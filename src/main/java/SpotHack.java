2af8886abedd39844eeb0cc37dbe171b7aa561bd
jphall93 <jph4@sunyorange.edu>
2015-09-26 23:07:42 -0400

    Initial commit

---
+++ src/main/java/SpotHack.java
@@ -0,0 +1,9 @@
+/**
+ * Created by JP on 9/26/2015.
+ */
+import static spark.Spark.*;
+public class SpotHack {
+    public static void main(String[] args) {
+        get("/", (req, res) -> "Hello, World!");
+    }
+}
